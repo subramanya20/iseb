3cdea84da4c41835d2a3e6d8fc3e7575be648527
subramanya20 <83331261+subramanya20@users.noreply.github.com>
2025-03-04 15:24:11 +0530

    Update and rename MyClass.java to new.java

--- new.java
+++ new.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; // Import the Scanner class
 
-class MyClass {
+class new {
   public static void main(String[] args) {
     int x, y, sum;
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
